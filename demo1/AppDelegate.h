4d409f4fe9190a6fff9d127f6a0a3132420dcdc3
Anil-Solanki <anilsolanki@anils-macbook-pro.local>
2016-11-19 22:01:59 +0530

    first commit

---
+++ demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by Anil Solanki on 19/11/16.
+//  Copyright © 2016 devlopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by Anil Solanki on 19/11/16.
+//  Copyright © 2016 devlopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
